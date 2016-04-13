ae7442244c502503b5c2bde47252df31ef3c1953
Boris Betzholz <boris@compu-ag.de>
2016-04-13 11:27:16 +0200

    me is the second

--- web/index.php
+++ web/index.php
@@ -8,6 +8,11 @@ $app = new Silex\Application();
 $app->get('/hello/{name}', function ($name) use ($app) {
 	return 'Hello '.$app->escape($name);
 });
+
+$app->get('/go0dbye/{name}', function ($name) use ($app) {
+	return 'Hello '.$app->escape($name);
+});
+
 $app->get('/', function () use ($app) {
 	return $app->redirect('/hello/world');
 });
